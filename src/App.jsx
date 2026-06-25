dbfbaf68e6f503d653a9e5559cbabb760b86ebe7
shivam_raj <shivamraj81020051@gmail.com>
2026-06-25 20:02:56 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+/**
+Day 114 – Mini Project: Full-Stack Notes App
+
+Task:
+Backend (Node + MongoDB)
+Connect with frontend (React)
+Deploy live
+ */
+
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [note, setNote] = useState("");
+  const [notes, setNotes] = useState([]);
+
+  // Fetch all notes from backend
+  const fetchNotes = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/notes");
+      setNotes(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Add note to backend
+  const addNote = async () => {
+    if (!note.trim()) return;
+
+    try {
+      await axios.post("http://localhost:5000/notes", {
+        title: note
+      });
+
+      setNote("");
+      fetchNotes();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Delete note from backend
+  const deleteNote = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/notes/${id}`);
+      fetchNotes();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  return (
+    <div>
+      <h1>Notes App</h1>
+
+      <input
+        value={note}
+        onChange={(e) => setNote(e.target.value)}
+        placeholder="Enter note"
+      />
+
+      <button onClick={addNote}>Add</button>
+
+      {notes.map((item) => (
+        <div key={item._id}>
+          {item.title}
+          <button onClick={() => deleteNote(item._id)}>
+            Delete
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
+
+
+/**
+Now flow is:
+Frontend (React)
+   ↓
+Axios
+   ↓
+Backend (Node)
+   ↓
+MongoDB Atlas
+ */
